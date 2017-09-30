571c184027c99168653683bd27d715654bfd344f
浩睿 <jiangshui.yjs@alibaba-inc.com>
2017-09-30 08:18:49 +0800

    feat: finish the first version code.

---
+++ index.js
@@ -0,0 +1,65 @@
+const REG_KEY = /\[(['"a-zA-Z0-9]*)\]/i;
+
+/**
+ * getter
+ * @param {object} data the object data
+ * @param {string} path the key path you want to get
+ * @param {object} options another options
+ */
+function getter(data, path, options = {}) {
+  const pathArr = path.split('.');
+  const result = pathArr.reduce(
+    (result, currentPath, currentIndex) => {
+      if (!result.errorPath) {
+        // get value
+        let key = currentPath;
+        if (REG_KEY.test(currentPath)) {
+          const nextKey = currentPath.match(REG_KEY)[1];
+          const currentKey = key.replace(nextKey, '').replace('[]', '');
+          result.value = result.value[currentKey];
+          key = nextKey;
+        }
+        result.value = result.value[key];
+
+        // check value
+        if (currentIndex !== pathArr.length - 1) {
+          // can not get the next value
+          const currentValueType = Object.prototype.toString.call(result.value);
+          if (/String|Number|Boolean|Null|Undefined/.test(currentValueType)) {
+            result.errorPath = currentPath;
+          }
+        }
+      }
+
+      return result;
+    },
+    { value: data, errorPath: null }
+  );
+
+  // throw error
+  if (
+    options.errorCallback &&
+    Object.prototype.toString.call(options.errorCallback) ===
+      '[object Function]'
+  ) {
+    options.errorCallback(result.errorPath, pathArr);
+  }
+  return result.value;
+}
+
+/**
+ * put the happy getter to all object
+ * @param {string} name custom getter function name
+ */
+function attachToObject(name = 'getter') {
+  if (!Object.prototype.hasOwnProperty(name)) {
+    Object.defineProperty(Object.prototype, name, {
+      value: function(path, options) {
+        return getter(this, path, options);
+      }
+    });
+  }
+}
+
+export default getter;
+export { attachToObject };

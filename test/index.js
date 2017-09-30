0c7549e3e047f6a3b2832659d1bde6a029942398
浩睿 <jiangshui.yjs@alibaba-inc.com>
2017-09-30 09:22:55 +0800

    feat: init test case

--- test/index.js
+++ test/index.js
@@ -1,8 +1,34 @@
-var assert = require('assert');
-describe('Array', function() {
-  describe('#indexOf()', function() {
-    it('should return -1 when the value is not present', function() {
-      assert.equal(-1, [1, 2, 3].indexOf(4));
-    });
+import assert from 'assert';
+import getter from '../lib/index.js';
+
+const testData = {
+  a: {
+    b: {
+      c: ['1', '2', '3'],
+      c1: null,
+      c2: undefined
+    },
+    b1: [
+      {
+        c: [
+          {
+            d: 1
+          }
+        ],
+        c1: function() {}
+      }
+    ]
+  }
+};
+
+describe('Correct data', function() {
+  it('should return 1', function() {
+    assert.equal(1 === getter(testData, 'b1[0].c[0].d'));
   });
 });
+
+// describe('Incorrect data', function() {
+//   it('should return 1', function() {
+//     assert.equal(1);
+//   });
+// });
